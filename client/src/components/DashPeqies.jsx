d66cd92c2748be1665abf9a37dcef71591ebd242
varletint <intvarlet@gmail.com>
2024-12-20 17:43:19 +0100

    list

--- client/src/components/DashPeqies.jsx
+++ client/src/components/DashPeqies.jsx
@@ -1,5 +1,38 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 export default function DashPeqies() {
-  return <div className='bg-red-50'>DashPeqies</div>;
+  const [peqies, setPeqies] = useState([]);
+
+  useEffect(() => {
+    const fetchPeqies = async () => {
+      const res = await fetch(`/api/post/getpeqs`);
+      if (!res.ok) {
+        alert("Failed to retrieve information");
+        return;
+      }
+      if (res.ok) {
+        const data = await res.json();
+        setPeqies(data.peqs);
+      }
+    };
+    fetchPeqies();
+  }, [peqies]);
+  return (
+    <div className=' px-3 py-6'>
+      {/* DashPeqies */}
+      <h1 className='text-center font-medium text-lg'>My Peqies</h1>
+
+      {peqies && peqies.length > 0 ? (
+        <ul className='peqies-list'>
+          {peqies.map((peq) => (
+            <li className='flex items-center gap-[100px] '>
+              {peq.courseTitle}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <li className='flex items-center gap-[10px] '> No peqies</li>
+      )}
+    </div>
+  );
 }
