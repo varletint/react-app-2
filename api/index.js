26c036b97889e8b72f5fd5b55a5f5137b2311d8c
varletint <intvarlet@gmail.com>
2025-03-12 04:25:14 +0100

    okay not ready

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import Peq from "./models/post.model.js";
+import { errorHandler } from "./utilities/error.js";
 
 import path from "path";
 
@@ -41,8 +43,51 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-app.get("/api", (req, res) => {
+app.get("/api", async (req, res, next) => {
   res.send("Hello World!");
+
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const peqs = await Peq.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.department && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { courseTitle: { $regex: req.query.searchTerm, $options: "i" } },
+          { courseCode: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalPeqs = await Peq.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthPeqs = await Peq.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      peqs,
+      totalPeqs,
+      lastMonthPeqs,
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 //   Server connection
 app.listen(3030, () => {
